e6616ff00859e672ca8140200befe56bacfe18e2
Futafi <D.futagi@vivaldi.net>
2019-07-19 02:12:51 +0900

    adapt to win and llinux

--- .virtualenv-auto-activate.sh
+++ .virtualenv-auto-activate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# virtualenv-auto-activate.sh
+# .virtualenv-auto-activate.sh
 # 
 # Installation:
 #   Add this line to your .bashrc or .bash-profile:
 #
-#       source /path/to/virtualenv-auto-activate.sh
+#       source /path/to/.virtualenv-auto-activate.sh
 #
 #   Go to your project folder, run "virtualenv .venv", so your project folder
 #   has a .venv folder at the top level, next to your version control directory.
@@ -25,16 +25,23 @@
 #
 #   The virtualenv will be activated automatically when you enter the directory.
 #
-#                   https://gist.github.com/codysoyland/2198913
+#      https://gist.github.com/Futafi/0cdbae47b748a9711cfe439ebaede902
 #
 _virtualenv_auto_activate() {
     if [ -e ".venv" ]; then
         # Check to see if already activated to avoid redundant activating
-        if [ "$VIRTUAL_ENV" != "$(pwd -P)/.venv" ]; then
+        # TODO now only availeble in C:\...
+        if [ "$VIRTUAL_ENV" != "$(pwd -P)/.venv" -a \
+          "$(echo $VIRTUAL_ENV | sed -e 's/\\/\//g' | sed 's/^C:/\/c/g')" != "$(pwd -P)/.venv" ] ; then
             _VENV_NAME=$(basename `pwd`)
             echo Activating virtualenv \"$_VENV_NAME\"...
             VIRTUAL_ENV_DISABLE_PROMPT=1
-            source .venv/bin/activate
+            if [ -e ".venv/bin" ]; then
+              source .venv/bin/activate
+            fi
+            if [ -e ".venv/Scripts" ]; then
+              source .venv/Scripts/activate
+            fi
             _OLD_VIRTUAL_PS1="$PS1"
             PS1="($_VENV_NAME)$PS1"
             export PS1
@@ -43,3 +50,4 @@ _virtualenv_auto_activate() {
 }
 
 export PROMPT_COMMAND=_virtualenv_auto_activate
+
