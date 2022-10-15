3db6460f73007b4c4d8573ad33b165e8b1a0e3ed
Futafi <D.futagi@vivaldi.net>
2022-10-15 22:44:23 +0900

    [update] install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,27 @@
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 DOT_DIR="$SCRIPT_DIR/dotfiles"
 DOTFILES="$(find $DOT_DIR -maxdepth 1 -mindepth 1 -printf "%f\n")"
+
 for f in $DOTFILES;
 do
   ln -snf $DOT_DIR/"$f" $HOME/"$f"
   echo "installed $f"
 done
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  if type "apt" > /dev/null 2>&1; then
+    INSTALLCMD="sudo apt install -y "
+    sudo apt update
+  elif type "pacman" >/dev/null 2>/&1; then
+    sudo pacman -S --needed --noconfirm base-devel git
+    git clone https://aur.archlinux.org/paru.git
+    sh -c "cd paru && makepkg -rsi"
+    INSTALLCMD="sudo paru -S --noconfirm "
+  fi
+  $INSTALLCMD vim htop tig git curl wget screen bash-completion man tree ncdu ranger
+  # TODO install code, github-cli
+fi
+
+vim +PlugInstall +qall
+
+source $HOME/.bashrc
